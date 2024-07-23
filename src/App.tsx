12b498fb31baf7bf7e82d571be0a1af59104bbd5
gabriellrraposo <gabriellucasrosaraposo@gmail.com>
2024-07-23 10:15:03 -0300

    Cria componentes de diferentes formas e estilos

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,77 @@
-import { useState } from 'react'
 import './App.css'
 
+// Define o tipo de Título
 type TitleProps = {
   text : string
+  size? : "small" | "large"
 }
 
+//Define o tipo de Texto
+type TextoProps = {
+  children : React.ReactNode
+}
+
+// Componente Título usando o tipo Título e tamanho grande
 const Title = (titulo : TitleProps) => {
-  return <h1>{titulo.text}</h1>
+  return <h1 
+    style={{ fontSize: titulo.size === "small" ? "3rem" : "5rem"}}
+  >{titulo.text}</h1>
 }
 
-const Title2 = ({ text} : { text : string }) => {
-  return <h1>{text}</h1>
+// Componente Título usando o tipo Título e tamanho pequeno
+const TitlePequeno = (titulo : TitleProps) => {
+  return <h1 
+    style={{fontSize: titulo.size === "small" ? "3rem" : "5rem"}}
+  >{titulo.text}</h1>
 }
 
+// Componente Título usando a desestruturação do tipo Título e tamanho padrão como small
+const TitleDefault = ({text, size = "small"} : TitleProps) => {
+  return <h1
+    style={{fontSize: size === "small" ? "3rem" : "5rem"}}
+    >{text}
+  </h1>
+}
+
+// Componente usando o desestruturação para definir o tipo
+const Subtitulo = ({ text } : { text : string }) => {
+  return <h2>{text}</h2>
+}
+
+const Paragrafo = ({ children } : { children : string}) => {
+  return <p>{children}</p>
+}
+
+// Componente usando o desestruturação para definir o tipo
+// Children é uma palavra reservada e representa o elemento filho deste componente
+// React.ReactNode é o tipo para html
+const Texto = ({ children } : { children : React.ReactNode }) => {
+  return <div>{ children }</div>
+}
+
+const OutroTexto = ({ children } : TextoProps) => {
+  return <div>{ children }</div>
+}
+
+// Chama os componentes que foram criados
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <div className='App'>
-      <Title text='Hello World'></Title>
-      <Title2 text="Hello World"></Title2>
+      <Title text='Este é o título' size='large'></Title>
+      <TitlePequeno text='Este é o título pequeno' size='small'></TitlePequeno>
+      <TitleDefault text='Small é o tamanho padrão para o título'></TitleDefault>
+      <Subtitulo text="Este é o subtitulo"></Subtitulo>
+      <Paragrafo>Este é um parágrafo</Paragrafo>
+      <Texto>
+        <span>
+          Este é um <b>ReactNode</b>
+        </span>
+      </Texto>
+      <OutroTexto>
+        <span>
+          Este é um outro <b>ReactNode</b>
+        </span>
+      </OutroTexto>
     </div>
   )
 }
